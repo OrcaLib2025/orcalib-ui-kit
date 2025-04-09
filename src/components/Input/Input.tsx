2974e42ddcbf59119851eeef6ddc779db56e070f
Denis Kolesnikov <deniskoleso2004@gmail.com>
2025-04-09 23:16:39 +0600

    feat: пофиксил баги

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -9,6 +9,8 @@ export const Input: React.FC<InputProps> = ({
     label,
     placeholder,
     value,
+    min,
+    max,
     onChange,
     size = 'default',
     theme,
@@ -23,6 +25,8 @@ export const Input: React.FC<InputProps> = ({
             <div className={cl['input-wrapper']}>
                 <input
                     type={type}
+                    min={min ? min : undefined}
+                    max={max ? max : undefined}
                     className={classnames(cl['input'], cl[`input-${size}`], cl[`input__theme-${theme}`], { [cl['input-focused']]: !!onFocus })}
                     placeholder={placeholder}
                     value={value}
--- src/utils/models/input.ts
+++ src/utils/models/input.ts
@@ -1,10 +1,12 @@
 export interface InputProps {
     onFocus?: () => void;
     clearable?: boolean;
-    type?: 'text' | 'password' | 'email' | 'number';
+    type?: 'text' | 'password' | 'email' | 'number' | 'range';
+    min?: number;
+    max?: number;
     label?: string;
     placeholder?: string;
-    value: string;
+    value: string | number | readonly string[] | undefined
     onChange: (value: string) => void;
     size?: 'default' | 'large';
     theme: 'light' | 'dark';
